1ba46a129098f95354b877df236e5be420543583
babloo1506 <suresh.shukla1984@gmail.com>
2017-11-17 12:45:42 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {render} from "react-dom";
+import App  from "./app/components/App";
+
+render(<App></App>,
+        document.getElementById("root") //browser/real dom 
+)
